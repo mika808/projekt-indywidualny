eee60b5de7e97ba60097eea0b93845c2682dc54b
Dominika Śliwińska <desliwinska@gmail.com>
2018-11-11 20:27:12 +0100

    Prepare top navigation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function showMenu() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("sidebarNav");
     if (x.className === "sidebar") {
         x.className += " responsive";
     } else {
         x.className = "sidebar";
     }
 }
+// -------------------------------------------------------
+
+// -------------------------------------------------------
