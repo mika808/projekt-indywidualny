15ce44d4ff8aecc3f5c62f995257a19003b60af3
Dominika Śliwińska <desliwinska@gmail.com>
2018-11-24 03:06:48 +0100

    Preapare cancel/quit modal and login modal

--- js/script.js
+++ js/script.js
@@ -9,4 +9,34 @@ function showMenu() {
     }
 }
 
-iconHam.addEventListener("click", showMenu)
+iconHam.addEventListener("click", showMenu);
+
+
+// -M-O-D-A-L-E-
+
+document.getElementById("show-exit-btn1").addEventListener("click", function(e) {
+    e.preventDefault();
+    var idModal = this.dataset.target;
+    document.getElementById(idModal).style.display = "flex";
+  });
+  
+  document.getElementById("show-exit-btn2").addEventListener("click", function(e) {
+    e.preventDefault();
+    var idModal = this.dataset.target;
+    document.getElementById(idModal).style.display = "flex";
+  });
+
+  document.getElementById("cancelBtn").addEventListener("click", function() {
+    var idModal = this.dataset.target;
+    document.getElementById(idModal).style.display = "none";
+  });
+
+  document.getElementById("quitBtn").addEventListener("click", function() {
+    var idModal = this.dataset.target;
+    document.getElementById(idModal).style.display = "none";
+  });
+
+  document.getElementById("enterBtn").addEventListener("click", function() {
+    var idModal = this.dataset.target;
+    document.getElementById(idModal).style.display = "none";
+  });
