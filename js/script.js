f19b2efc98af2b5b4ea2378abd4040ba53d52c20
Dominika Śliwińska <desliwinska@gmail.com>
2018-12-09 18:46:27 +0100

    Prepare section Postback

--- js/script.js
+++ js/script.js
@@ -2,62 +2,63 @@
 var iconHam = document.querySelector(".icon-hamburger");
 
 function showMenu() {
-    var x = document.getElementById("sidebarNav");
-    if (x.className === "sidebar") {
-        x.className += " responsive";
-    } else {
-        x.className = "sidebar";
-    }
+  var x = document.getElementById("sidebarNav");
+  if (x.className === "sidebar") {
+    x.className += " responsive";
+  } else {
+    x.className = "sidebar";
+  }
 }
 
 iconHam.addEventListener("click", showMenu);
 
-
 // -M-O-D-A-L-E-
 
-document.querySelectorAll(".show-modal").forEach(
-    function(element){
-        element.addEventListener(
-            "click", function(e){
-                e.preventDefault();
-                showModal(this);
-            }
-        )
-    }
-)
+document.querySelectorAll(".show-modal").forEach(function(element) {
+  element.addEventListener("click", function(e) {
+    e.preventDefault();
+    showModal(this);
+  });
+});
 
 function showModal(elem) {
-    var idModal= elem.dataset.target;
-    document.getElementById(idModal).style.display = "flex";
+  var idModal = elem.dataset.target;
+  document.getElementById(idModal).style.display = "flex";
 }
 
-
-document.querySelectorAll(".close-modal").forEach(
-    function(element){
-        element.addEventListener(
-            "click", function(e){
-                e.preventDefault();
-                closeModal(this);
-            }
-        )
-    }
-)
+document.querySelectorAll(".close-modal").forEach(function(element) {
+  element.addEventListener("click", function(e) {
+    e.preventDefault();
+    closeModal(this);
+  });
+});
 
 function closeModal(elem) {
-    var idModal= elem.dataset.target;
-    document.getElementById(idModal).style.display = "none";
+  var idModal = elem.dataset.target;
+  document.getElementById(idModal).style.display = "none";
 }
 
+document.addEventListener("keyup", function(e) {
+  if (e.keyCode === 27) {
+    document.querySelectorAll(".modal").forEach(function(element) {
+      element.style.display = "none";
+    });
+  }
+});
 
-document.addEventListener(
-    'keyup', function(e){
-        if(e.keyCode === 27){
-            document.querySelectorAll(".modal").forEach(
-                function(element){
-                    element.style.display = "none";
-                }
-            )
-        }
-    }
-)
+function prepareSlider(input) {
+  var label = input.parentElement.querySelector("#slider-value-label");
+  label.innerHTML = input.value + " hours";
+  var inputMin = input.getAttribute("min");
+  var inputMax = input.getAttribute("max");
+  var unit = (inputMax - inputMin) / 100;
+  var percent = (input.value - inputMin) / unit;
+  input.style.setProperty("--value", percent);
+}
 
+var sendAfterSlider = document.getElementById("send-after-slider");
+prepareSlider(sendAfterSlider);
+sendAfterSlider.addEventListener("change", function(evt) {
+  var input = evt.target;
+  prepareSlider(input);
+});
